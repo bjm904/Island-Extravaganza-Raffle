e432dc63925b89d97125f955738d63a573a87e85
Bryce Meyer <bjm904@gmail.com>
2013-12-04 02:25:51 -0800

    Initial Commit

---
+++ Prizes.js
@@ -0,0 +1,87 @@
+var pri=[[["","                                          ~~~   Grand Prize   ~~~","",false],
+
+
+
+
+
+//START GRAND PRIZE  \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/
+
+["","                                                   Bert J Meyer","",false],
+
+//STOP /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ 
+
+
+
+
+
+["","","",false],
+["","","",false]],[
+["","                                                      7:00 Raffle","",false],
+
+
+
+// [  Prize number  ,  Prize Name  ,  Winner  ,  Has Picked Up?  ]
+
+//START 7:00pm RAFFLE  \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/
+
+[701,"Snowboard","Bob ",false],
+[702,"Gift Certificate to A Plus, $85 ","John Copenhagen",true],
+[703,"Gift Certificate to Robertos, $25","Angela Meyer",false],
+[704,"Tshirt, gloves and gaiter ","Jason",false],
+[705,"Gift card for vons","Fluttershy",false],
+[706,"Tshirt","Mika",false],
+[707,"You are awesome gift card","Wilbert",false],
+[708,"Gift Certificate to Bonnie's Pets, $20","Steve",false],
+[709,"iPad Air","Diego Pezoa",false],
+[710,"iPhone 5s","Vicente Reyes",false],
+[711,"iPhone 5s 500TB","Diego Pezoa",false],
+
+//STOP /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ 
+
+
+
+
+
+["","","",false]],
+[["","                                                      8:00 Raffle","",false],
+
+
+
+
+
+//START 8:00pm RAFFLE  \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/
+
+[801,"Gift Certificate to Oak Tree, $25","Hector",true],
+[802,"MMSA Tshirt","George",false],
+[803,"iPhone case","Kat",false],
+[804,"Gravis backpack","Tim",false],
+[805,"Analog, Scott raffle bag","Andreas",false],
+
+//STOP /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ 
+
+
+
+
+
+["","","",false]],
+[["","                                                      9:00 Raffle","",false],
+
+
+
+
+
+//START 9:00pm RAFFLE  \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/ \/
+
+[901,"Tshirt, scarf, and wallet","Mike",true],
+[902,"goggles, hat, and Tshirt","Joe",true],
+[903,"kitties","Matt",false],
+[904,"Shoes, gloves, and Tshirt","Mark",false],
+
+//STOP /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ /\ 
+
+
+
+
+
+["","","",false]]
+];
---
+++ config.js
@@ -0,0 +1,5 @@
+var confSpeed = 24;
+var confShow = "all"; 
+var ScrollbarSpacingMultiplyer= 13;
+var color1="black";var color2="#CC9900";var color3="#FFCC00";
+var scrollbarConf = "Thank you to all our donors and volunteers: (List of doners here)";
