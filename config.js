f7f2c5612a5439b7f88d933efedc99b57b278053
Bryce Meyer <bjm904@gmail.com>
2013-12-11 12:18:11 -0800

    Make scrollbar not refresh

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-var confSpeed = 4;
+var confSpeedSpool = 4;
 var confShow = "all"; 
 var ScrollbarSpacingMultiplyer= 13;
 var color1="black";var color2="#CC9900";var color3="#FFCC00";
 var scrollbarConf = "Thank you to all our donors and volunteers: (List of doners here)";
+
+var confSpeedScroll = 4;
