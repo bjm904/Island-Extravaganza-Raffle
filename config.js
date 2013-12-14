a3caf736aedb4e6dcb19692f114789e44b793850
Bryce Meyer <bjm904@gmail.com>
2013-12-14 00:13:26 -0800

    Update info

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-var confSpeedSpool = 4;
-var confShow = "all"; 
-var ScrollbarSpacingMultiplyer= 13;
-var color1="black";var color2="#CC9900";var color3="#FFCC00";
-var scrollbarConf = "Thank you to all our donors and volunteers: (List of doners here)";
-
-var confSpeedScroll = 4;
+﻿var confSpeedSpool=22;var confSpeedScroll=0;var confShow='all'; var ScrollbarSpacingMultiplyer= 12;var color1='White';var color2='#400080';var color3='#52109A';var color4='White';var color5='Black';var scrollbarConf='Thank you to all our donors and volunteers: (List of doners here)';
